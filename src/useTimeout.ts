7258540d13561397bd7520389b6f6a7cc0fe9f00
saisumith770 <saisumith812@gmail.com>
2021-10-26 15:30:21 +0530

    built the timeout hook along with type safety

---
+++ src/useTimeout.ts
@@ -0,0 +1,48 @@
+import { useCallback, useEffect, useRef } from "react"
+
+type ReturnType = {
+    reset: () => void,
+    clear: () => void
+}
+
+/**
+ * 
+ * @param callback 
+ * @param delay 
+ * @returns TimeoutControls
+ * @description
+ * Hook to handle setTimeout events with extra properties by resetting and clearing them. 
+ * It also removes the event after component removal from DOM.
+ * @example
+ * const { reset, clear } = useTimeout<() => string>(() => "", 1000)
+ * reset()
+ * clear()
+ */
+export default function useTimeout<CallbackType extends () => void>(callback: CallbackType, delay: number): ReturnType {
+    const callbackRef = useRef<CallbackType>(callback)
+    const timeoutRef = useRef<typeof delay>()
+
+    useEffect(() => {
+        callbackRef.current = callback
+    }, [callback])
+
+    const set = useCallback(() => {
+        timeoutRef.current = setTimeout(() => callbackRef.current(), delay)
+    }, [delay])
+
+    const clear = useCallback(() => {
+        timeoutRef.current && clearTimeout(timeoutRef.current)
+    }, [])
+
+    useEffect(() => {
+        set()
+        return clear
+    }, [delay, set, clear])
+
+    const reset = useCallback(() => {
+        clear()
+        set()
+    }, [clear, set])
+
+    return { reset, clear }
+}
