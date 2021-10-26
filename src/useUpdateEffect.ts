293af24dd43096c91fa836211d4f7058d8f4c972
saisumith770 <saisumith812@gmail.com>
2021-10-26 15:50:18 +0530

    built useUpdate hook with type safety

---
+++ src/useUpdateEffect.ts
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react"
+
+/**
+ * 
+ * @param callback 
+ * @param dependencies 
+ * @description
+ * Hook that works like useEffect but doesn't run on first render of the component.
+ * @example
+ * const [count,setCount] = useState(0)
+ * useUpdateEffect(() => console.log("update worked"),[count])
+ */
+export default function useUpdateEffect<CallbackType extends () => void>(callback: CallbackType, dependencies: any[]) {
+    const firstRenderRef = useRef<boolean>(true)
+
+    useEffect(() => {
+        if (firstRenderRef.current) {
+            firstRenderRef.current = false
+            return
+        }
+        return callback()
+    }, dependencies)
+}
