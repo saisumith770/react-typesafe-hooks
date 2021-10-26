a0c7465c298888212c0225f69d97f502ec0915b3
saisumith770 <saisumith812@gmail.com>
2021-10-26 12:51:49 +0530

    added type safe useToggle hook

---
+++ src/useToggle.ts
@@ -0,0 +1,27 @@
+import { useState } from "react"
+
+type ToggleValues = boolean
+type ReturnType = [
+    ToggleValues,
+    (value?: ToggleValues) => void
+]
+
+/**
+ * @description
+ * Toggles between true or false
+ * @example
+ * const [value,toggler] = useToggle() //optional boolean parameter can be entered
+ * console.log(value)
+ * toggler() //optional parameter => toggler(false)
+ */
+export default function useToggle(defaultValue: ToggleValues = false): ReturnType {
+    const [value, setValue] = useState(defaultValue)
+
+    function toggleValue(value?: ToggleValues): void {
+        setValue(currentValue =>
+            typeof value === "boolean" ? value : !currentValue
+        )
+    }
+
+    return [value, toggleValue]
+}
