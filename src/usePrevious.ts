2f1072b8e5e570560502f677ec4b589dd38d4953
saisumith770 <saisumith812@gmail.com>
2021-10-27 07:59:53 +0530

    added jsdoc

--- src/usePrevious.ts
+++ src/usePrevious.ts
@@ -3,6 +3,15 @@ import { useRef } from "react"
 type StatefulValue = any
 type RefToPreviousValue = any
 
+/**
+ * @param value
+ * @description
+ * Hook to show the previous value of a stateful object.
+ * @example
+ * const [count,setCount] = useState(0)
+ * const prevVal = usePrevious(count)
+ * console.log(prevVal)
+ */
 export default function usePrevious(value: StatefulValue): RefToPreviousValue {
     const currentRef = useRef<StatefulValue>(value)
     const previousRef = useRef<StatefulValue>()
