df4a0801cf18010a7a6c759ddd527f4e0cd44c81
saisumith770 <saisumith812@gmail.com>
2021-10-29 22:42:44 +0530

    added hook to copy text to clipboard

---
+++ src/useCopyToClipboard.ts
@@ -0,0 +1,34 @@
+import { useState } from "react"
+
+/**
+ * 
+ * @returns ClipboardCopyControls
+ * @description
+ * Hook that copies the provided text to the clipboard.
+ * @example
+ * const [copyToClipboard, { success }] = useCopyToClipboard()
+ * copyToClipboard("This was copied")
+ */
+export default function useCopyToClipboard(): [
+  copyToClipboard: (text: string) => Promise<void>,
+  operators: {
+    value: string | undefined,
+    success: boolean | undefined
+  }
+] {
+  const [value, setValue] = useState<string>()
+  const [success, setSuccess] = useState<boolean>()
+
+  const copyToClipboard = async (text: string) => {
+    navigator.clipboard.writeText(text)
+      .then(() => {
+        setValue(text)
+        setSuccess(true)
+      })
+      .catch(() => {
+        setSuccess(false)
+      })
+  }
+
+  return [copyToClipboard, { value, success }]
+}
