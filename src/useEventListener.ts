bc5e8fd098a0f501b2f9df82001d7170761e3945
saisumith770 <saisumith812@gmail.com>
2021-10-29 23:10:15 +0530

    added useEventListener hook with type safety and jsdoc

---
+++ src/useEventListener.ts
@@ -0,0 +1,36 @@
+import { useEffect, useRef } from "react"
+
+type EventCallback = (this: Window, ev: any) => any
+
+/**
+ * 
+ * @param eventType 
+ * @param callback 
+ * @param element 
+ * @description
+ * Hook to add event listeners to the DOM.
+ * @example
+ * const [key, setKey] = useState("")
+  useEventListener("keydown", e => {
+    setKey(e.key)
+  })
+ */
+export default function useEventListener(
+    eventType: string,
+    callback: EventCallback,
+    element: Window | Document = window
+) {
+    const callbackRef = useRef(callback)
+
+    useEffect(() => {
+        callbackRef.current = callback
+    }, [callback])
+
+    useEffect(() => {
+        if (element == null) return
+        const handler = (e: any) => (callbackRef as any).current(e)//have to check typings of Ref object.
+        element.addEventListener(eventType, handler)
+
+        return () => element.removeEventListener(eventType, handler)
+    }, [eventType, element])
+}
