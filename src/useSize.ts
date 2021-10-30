51c51c3920a2c1e06935af3b2094f692f2b330d8
saisumith770 <saisumith812@gmail.com>
2021-10-30 18:25:52 +0530

    added useSize hook with type safety and jsdoc

---
+++ src/useSize.ts
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react"
+
+/**
+ * 
+ * @param ref 
+ * @returns ComponentRectPoints
+ * @description
+ * Hook that returns the size of the component when resized.
+ * @example
+ * const ref = useRef()
+  const size = useSize(ref)
+  console.log(size)
+ */
+export default function useSize(ref: React.MutableRefObject<any>) {
+    const [size, setSize] = useState<Record<string, any>>({})
+
+    useEffect(() => {
+        if (ref.current == null) return
+        const observer = new ResizeObserver(([entry]) => setSize(entry.contentRect))
+        observer.observe(ref.current)
+        return () => observer.disconnect()
+    }, [])
+
+    return size
+}
