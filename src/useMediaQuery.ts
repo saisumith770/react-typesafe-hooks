b93a7c00852b68d885d6f02c0a7ff4f94a364ab8
saisumith770 <saisumith812@gmail.com>
2021-10-30 18:09:53 +0530

    added useMediaQuery hook with type safety and jsdoc

---
+++ src/useMediaQuery.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react"
+import useEventListener from "./useEventListener"
+
+/**
+ * 
+ * @param mediaQuery 
+ * @returns MediaQuerySatisfiedState
+ * @description
+ * Hook that will check for the media query given and return its state.
+ * @example
+ * const isLarge = useMediaQuery("(min-width: 200px)")
+ * console.log(isLarge)
+ */
+export default function useMediaQuery(mediaQuery: string) {
+    const [isMatch, setIsMatch] = useState(false)
+    const [mediaQueryList, setMediaQueryList] = useState<MediaQueryList>()
+
+    useEffect(() => {
+        const list = window.matchMedia(mediaQuery)
+        setMediaQueryList(list)
+        setIsMatch(list.matches)
+    }, [mediaQuery])
+
+    useEventListener("change", e => setIsMatch(e.matches), mediaQueryList as any)//check typings
+
+    return isMatch
+}
