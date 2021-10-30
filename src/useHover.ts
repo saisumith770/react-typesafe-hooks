60b3214a7ad31ba941031f1bf89bce294a87e7f1
saisumith770 <saisumith812@gmail.com>
2021-10-30 18:00:55 +0530

    added useHover hook with type safety and jsdoc

---
+++ src/useHover.ts
@@ -0,0 +1,22 @@
+import { useState } from "react"
+import useEventListener from "./useEventListener"
+
+/**
+ * 
+ * @param ref 
+ * @returns HoveredState
+ * @description
+ * Hook that returns whether the component has hovered or not.
+ * @example
+ * const elementRef = useRef()
+  const hovered = useHover(elementRef)
+  console.log(hovered)
+ */
+export default function useHover(ref: React.MutableRefObject<any>) {
+  const [hovered, setHovered] = useState(false)
+
+  useEventListener("mouseover", () => setHovered(true), ref.current)
+  useEventListener("mouseout", () => setHovered(false), ref.current)
+
+  return hovered
+}
